bf42fefee41cc84d15b5a023ee8550d28f13eb1e
Jonathan Sandoval Cardona <jsandoval@utp.edu.co>
2014-09-16 17:43:28 -0500

    Se adiciona la configuración para org.

---
+++ personal/org-config.el
@@ -0,0 +1,7 @@
+;; Configuración del modo org
+(custom-set-variables
+ '(org-agenda-files (quote
+                     ("~/personal/org-agenda/personal.org")
+                     ("~/personal/org-agenda/movimientos.org")
+                     ))
+)
