3ced20bd6a1a5c684112b559c888cc7c49dea0b7
Jonathan Sandoval <jsandoval@utp.edu.co>
2014-09-18 21:59:35 -0500

    Se configura la agenda. Pendiente de mover el directorio en el trabajo.

--- personal/org-config.el
+++ personal/org-config.el
@@ -1,7 +1,8 @@
 ;; Configuración del modo org
-(custom-set-variables
- '(org-agenda-files (quote
-                     ("~/personal/org-agenda/personal.org")
-                     ("~/personal/org-agenda/movimientos.org")
-                     ))
-)
+(setq org-agenda-files
+      (delq nil
+            (mapcar (lambda (x) (and (file-exists-p x) x))
+                    '("~/org/org-agenda/agenda.org"
+                      "~/org/org-agenda/ingresos.org"
+                      "~/org/org-agenda/movimientos.org"
+                      "~/org/org-agenda/personal.org"))))
