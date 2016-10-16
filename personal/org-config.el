3c3171d5d6315e052d2de3a0bc99d85bcd2763d8
Jonathan Sandoval Cardona <ark@debian>
2016-10-16 11:07:14 -0500

    Cambio de archivos de agenda.

--- personal/org-config.el
+++ personal/org-config.el
@@ -4,7 +4,4 @@
 (setq org-agenda-files
       (delq nil
             (mapcar (lambda (x) (and (file-exists-p x) x))
-                    '("~/org/org-agenda/agenda.org"
-                      "~/org/org-agenda/ingresos.org"
-                      "~/org/org-agenda/movimientos.org"
-                      "~/org/org-agenda/personal.org"))))
+                    '("~/repositorio/notas-org/analisis.org"))))
