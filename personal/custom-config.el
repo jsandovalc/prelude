a625716ca479b8433791add42721fabc57e9d55f
Jonathan Sandoval <jsandoval@utp.edu.co>
2018-02-12 16:04:07 -0500

    Custom config:
    - Hideshow enabled.
    - Recompile to f5.
    - Chooose between swiper and grep depending on size file.

--- personal/custom-config.el
+++ personal/custom-config.el
@@ -1,2 +1,27 @@
 (setq erc-autojoin-channels-alist
       '(("freenode.net" "#mediagoblin" )))
+
+(yas-global-mode 1)
+
+;; Swiper is too slow on big files like my org. Use counsel-grep
+;; instead.
+(defun ora-swiper ()
+  (interactive)
+  (if (and (buffer-file-name)
+           (not (ignore-errors
+                  (file-remote-p (buffer-file-name))))
+           (if (eq major-mode 'org-mode)
+               (> (buffer-size) 60000)
+             (> (buffer-size) 300000)))
+      (progn
+        (save-buffer)
+        (counsel-grep))
+    (swiper--ivy (swiper--candidates))))
+
+(global-set-key "\C-s" 'ora-swiper)
+
+;; hideshow for programming
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+
+;; Recompile
+(global-set-key (quote [f5]) 'recompile)
