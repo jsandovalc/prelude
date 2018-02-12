6b03497954987b0974098c32f69426dc4df5ac78
Jonathan Sandoval <jsandoval@utp.edu.co>
2018-02-12 17:26:06 -0500

    Add org-capture configuration.

---
+++ personal/custom-org.el
@@ -0,0 +1,15 @@
+(setq org-directory "~/repositorio/notas-org/")
+(setq org-list-allow-alphabetical t)
+
+(setq org-default-notes-file (concat org-directory "/agenda.org"))
+
+(define-key global-map "\C-cc" 'org-capture)
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "" "Tasks")
+         "* TODO %?\n %t\n %a\n %i")
+        ("c" "Tarea de la Corriente" entry (file+headline "" "Corriente")
+         "* TODO %?\n %t\n %a\n %i")
+        ("f" "New facturedo task" entry
+         (file+olp "" "Facturedo" "Tareas")
+         "*** TODO %?\n %i\n %a" :jump-to-captured t :empty-lines 1)))
