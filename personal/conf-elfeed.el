c42735ca66c1eeb0af03ff72f7c94922b4e095d8
Jonathan Sandoval Cardona <ark@gaia>
2018-02-28 23:43:50 -0500

    Add new feed.

--- personal/conf-elfeed.el
+++ personal/conf-elfeed.el
@@ -1,4 +1,4 @@
 (setq elfeed-feeds
       '("http://planet.emacsen.org/atom.xml"
-        "http://rss.slashdot.org/Slashdot/slashdot"
-        "http://feeds.feedburner.com/ENTERCO"))
+        "http://feeds.feedburner.com/ElLadoDelMal"
+        ))
