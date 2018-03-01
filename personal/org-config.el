f786800100c949f5b5d3dd7f01a5ebf12e3211af
Jonathan Sandoval Cardona <ark@gaia>
2018-02-28 23:45:15 -0500

    Org config.

--- personal/org-config.el
+++ personal/org-config.el
@@ -1,10 +1,15 @@
 ;; Configuración del modo org
+(require 'package)
+
 (global-set-key "\C-ca" 'org-agenda)
 
 (setq org-agenda-files
       (delq nil
             (mapcar (lambda (x) (and (file-exists-p x) x))
-                    '("~/repositorio/notas-org/agenda.org"))))
+                    '("~/notas-org/agenda.org"))))
 
 (setq org-clock-persist 'history)
 (org-clock-persistence-insinuate)
+
+; new repo
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
