aae5bee4c6c751f51f26c505367afaf371300542
Jonathan Sandoval Cardona <ark@gaia>
2018-02-28 23:45:01 -0500

    Mail config.

--- personal/mail.el
+++ personal/mail.el
@@ -5,3 +5,13 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (require 'org-notmuch)
+
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "cloudneozero@gmail.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      starttls-gnutls-program "/usr/bin/gnutls-cli"
+      starttls-extra-arguments nil
+      starttls-use-gnutls t)
